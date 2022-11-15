6b9e26125ce22f66b6b82fd31870bab014a0ca04
amit <aamitbiton@gmail.com>
2022-11-15 22:09:06 +0200

    basic

--- src/utils/urlGenerator.tsx
+++ src/utils/urlGenerator.tsx
@@ -1,8 +1,8 @@
 export const getCurrentConditionsUrl = (locationKey: string) =>
-  `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`;
+  `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}`;
 
 export const autoCompleteUrl =
-  "http://dataservice.accuweather.com/locations/v1/cities/autocomplete";
+  "https://dataservice.accuweather.com/locations/v1/cities/autocomplete";
 
 export const geoPositionSearchUrl =
-  "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
+  "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
